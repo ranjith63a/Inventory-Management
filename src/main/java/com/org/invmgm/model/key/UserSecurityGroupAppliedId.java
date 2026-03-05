e715ecac392d5eb01faefb56f1acfa7d074a4685
Ranjith Arumugam <ranjithvinu423423@gmail.com>
2026-03-06 00:07:00 +0530

    Bug fixed

--- src/main/java/com/org/invmgm/model/key/UserSecurityGroupAppliedId.java
+++ src/main/java/com/org/invmgm/model/key/UserSecurityGroupAppliedId.java
@@ -19,7 +19,7 @@ import java.util.Objects;
 public class UserSecurityGroupAppliedId implements Serializable {
 
     private Long userLoginId;
-    private Long groupId;
+    private String groupId;
     private LocalDateTime fromDate;
 
     @Override
