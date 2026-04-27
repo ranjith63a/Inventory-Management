04d3c301c451882fcae00d82adbb159f41fc4ef4
Ranjith Arumugam <ranjithvinu423423@gmail.com>
2026-04-27 23:24:03 +0530

    Added Inventory Transfer Seed data and entity and service

--- src/main/java/com/org/invmgm/repository/InventoryTransferRepository.java
+++ src/main/java/com/org/invmgm/repository/InventoryTransferRepository.java
@@ -1,9 +1,9 @@
 package com.org.invmgm.repository;
 
-import com.org.invmgm.model.InventoryItemTransfer;
+import com.org.invmgm.model.InventoryTransfer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface InventoryItemTransferRepository extends JpaRepository<InventoryItemTransfer, Long> {
+public interface InventoryTransferRepository extends JpaRepository<InventoryTransfer, Long> {
 }
